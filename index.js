a95bee10815edbcb39de0e94a91c7095bab83fce
Niv Sardi <xaiki@evilgiggle.com>
2016-03-10 11:07:14 -0300

    trakt watchlist
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- index.js
+++ index.js
@@ -1,213 +1,224 @@
-'use strict';
-
-var GenericProvider = require('butter-provider');
-var querystring = require('querystring');
-var Q = require('q');
-var request = require('request');
-var inherits = require('util').inherits;
 var _ = require('lodash');
-var Datastore = require('nedb');
+var moment = require('moment');
+var localStorage = localStorage || require('localStorage');
+var Trakt = require('trakt.tv');
 
-var apiUrl = 'http://butter.vodo.net/popcorn',
-    db = new Datastore();
-
-function Vodo() {
-    if (!(this instanceof Vodo)) {
-        return new Vodo();
-    }
-
-    GenericProvider.call(this);
-}
-inherits(Vodo, GenericProvider);
-
-Vodo.prototype.config = {
-    name: 'vodo',
-    uniqueId: 'imdb_id',
-    tabName: 'Vodo',
-    type: 'movie',
-    /* should be removed */
-    //subtitle: 'ysubs',
-    metadata: 'trakttv:movie-metadata'
-};
-
-function formatForButter(items) {
-    var results = {};
-    var movieFetch = {};
-    movieFetch.results = [];
-    movieFetch.hasMore = (Number(items.length) > 1 ? true : false);
-    _.each(items, function (movie) {
-        if (movie.Quality === '3D') {
-            return;
-        }
-        var imdb = movie.ImdbCode;
-
-        // Calc torrent health
-        var seeds = 0; //XXX movie.TorrentSeeds;
-        var peers = 0; //XXX movie.TorrentPeers;
-
-        var torrents = {};
-        torrents[movie.Quality] = {
-            url: movie.TorrentUrl,
-            size: movie.SizeByte,
-            filesize: movie.Size,
-            seed: seeds,
-            peer: peers
-        };
-
-        var ptItem = results[imdb];
-        if (!ptItem) {
-            ptItem = {
-                imdb_id: imdb,
-                title: movie.MovieTitleClean.replace(/\([^)]*\)|1080p|DIRECTORS CUT|EXTENDED|UNRATED|3D|[()]/g, ''),
-                year: movie.MovieYear,
-                genre: [movie.Genre],
-                rating: movie.MovieRating,
-                image: movie.CoverImage,
-                cover: movie.CoverImage,
-                backdrop: movie.CoverImage,
-                torrents: torrents,
-                subtitle: {}, // TODO
-                trailer: false,
-                synopsis: movie.Synopsis || 'No synopsis available.',
-                type: 'movie'
-            };
-
-            movieFetch.results.push(ptItem);
-        } else {
-            _.extend(ptItem.torrents, torrents);
-        }
-
-        results[imdb] = ptItem;
+var Watchlist = function (args) {
+    args = args || {}
+    this.cacheTime = args.cacheTime || 14400000;
+    this.trakt = args.trakt || new Trakt({
+        client_id: args.id || '647c69e4ed1ad13393bf6edd9d8f9fb6fe9faf405b44320a6b71ab960b4540a2',
+        client_secret: args.secret || 'f55b0a53c63af683588b47f6de94226b7572a6f83f40bd44c58a7c83fe1f2cb1',
+        plugins: ['ondeck']
     });
 
-    return movieFetch.results;
-}
-
-Vodo.prototype.extractIds = function (items) {
-    return _.pluck(items.results, 'imdb_id');
-};
-
-Vodo.prototype.updateAPI = function () {
-    var self = this;
-    var defer = Q.defer();
-    console.info('Request to Vodo', apiUrl);
-    request({
-        uri: apiUrl,
-        strictSSL: false,
-        json: true,
-        timeout: 10000
-    },
-            function (err, res, data) {
-                /*
-                  data = _.map (helpers.formatForButter(data), function (item) {
-                  item.rating = item.rating.percentage * Math.log(item.rating.votes);
-                  return item;
-                  });
-                */
-                db.insert(formatForButter(data.downloads), function (err, newDocs) {
-                    if (err) {
-                        console.error('Vodo.updateAPI(): Error inserting', err);
-                    }
-
-                    db.find({}).limit(2).exec(function (err, docs) {
-                        //console.debug('FIND ---->', err, docs);
-                    });
-                    defer.resolve(newDocs);
-                });
-            });
-
-    return defer.promise;
-};
-
-Vodo.prototype.fetch = function (filters) {
-    var self = this;
-    if (!self.fetchPromise) {
-        self.fetchPromise = this.updateAPI();
-    }
-
-    var defer = Q.defer();
-    var params = {
-        sort: 'rating',
-        limit: 50
+    this.trakt.checkButterIsAuth = args.checkTrakt || function () {
+        console.error("you did not supply a trakt auth function")
     };
-    var findOpts = {};
-
-    if (filters.keywords) {
-        findOpts = {
-            title: new RegExp(filters.keywords.replace(/\s/g, '\\s+'))
-        };
-    }
-
-    if (filters.genre) {
-        params.genre = filters.genre;
-    }
-
-    if (filters.order) {
-        params.order = filters.order;
-    }
-
-    if (filters.sorter && filters.sorter !== 'popularity') {
-        params.sort = filters.sorter;
-    }
-
-    var sortOpts = {};
-    sortOpts[params.sort] = params.order;
-
-    self.fetchPromise.then(function () {
-        db.find(findOpts)
-            .sort(sortOpts)
-            .skip((filters.page - 1) * params.limit)
-            .limit(Number(params.limit))
-            .exec(function (err, docs) {
-                docs.forEach(function (entry) {
-                    entry.type = 'movie';
-                });
-
-                return defer.resolve({
-                    results: docs,
-                    hasMore: docs.length ? true : false
-                });
-            });
-    });
-
-    return defer.promise;
 };
 
-Vodo.prototype.random = function () {
-    var defer = Q.defer();
+Watchlist.prototype.constructor = Watchlist;
+Watchlist.prototype.config = {
+    name: 'Watchlist'
+};
 
-    function get(index) {
-        var options = {
-            uri: apiUrl + Math.round((new Date()).valueOf() / 1000),
-            json: true,
-            timeout: 10000
-        };
-        var req = _.extend(true, {}, apiUrl[index], options);
-        request(req, function (err, res, data) {
-            if (err || res.statusCode >= 400 || (data && !data.data)) {
-                console.error('Vodo API endpoint \'%s\' failed.', apiUrl);
-                if (index + 1 >= apiUrl.length) {
-                    return defer.reject(err || 'Status Code is above 400');
-                } else {
-                    get(index + 1);
-                }
-                return;
-            } else if (!data || data.status === 'error') {
-                err = data ? data.status_message : 'No data returned';
-                return defer.reject(err);
+var rearrange = function (items) {
+    var no_arrange = [],
+        arrange = [],
+        arranged;
+
+    return Promise.all(items.map(function (item) {
+        if (item) {
+            if (item.first_aired) {
+                arrange.push(item);
             } else {
-                return defer.resolve(Common.sanitize(data.data));
+                no_arrange.push(item);
             }
+        }
+    })).then(function () {
+        arranged = arrange.sort(function(a, b){
+            if(a.episode_aired > b.episode_aired) {
+                return -1;
+            }
+            if(a.episode_aired < b.episode_aired) {
+                return 1;
+            }
+            return 0;
         });
-    }
-    get(0);
-
-    return defer.promise;
+        console.log('rearranged shows by air date');//debug
+        return arranged.concat(no_arrange);
+    });
 };
 
-Vodo.prototype.detail = function (torrent_id, old_data) {
-    return Q(old_data);
+var format = function (items) {
+    var itemList = [];
+    console.log('format'); //debug
+
+    return Promise.all(items.map(function (item) {
+        if (item.next_episode) {
+            if(moment(item.next_episode.first_aired).fromNow().indexOf('in') !== -1) {
+                console.warn('"%s" is not released yet, not showing', item.show.title + ' ' + item.next_episode.season + 'x' + item.next_episode.number);
+            } else {
+                var show = item.show;
+                show.type = 'show';
+                show.episode = item.next_episode.number;
+                show.season = item.next_episode.season;
+                show.episode_title = item.next_episode.title;
+                show.episode_id = item.next_episode.ids.tvdb;
+                show.episode_aired = item.next_episode.first_aired;
+                show.imdb_id = item.show.ids.imdb;
+                show.tvdb_id = item.show.ids.tvdb;
+                show.image = item.show.images.poster.thumb;
+                show.rating = item.show.rating;
+                show.title = item.show.title;
+                show.trailer = item.show.trailer;
+
+                itemList.push(show);
+            }
+        } else {
+            if (!item.movie) {
+                console.log('item is not a movie', item); //debug
+            } else {
+                if(moment(item.movie.released).fromNow().indexOf('in') !== -1) {
+                    console.warn('"%s" is not released yet, not showing', item.movie.title);
+                } else {
+                    var movie = item.movie;
+                    movie.type = 'movie';
+                    movie.imdb_id = item.movie.ids.imdb;
+                    movie.rating = item.movie.rating;
+                    movie.title = item.movie.title;
+                    movie.trailer = item.movie.trailer;
+                    movie.year = item.movie.year;
+                    movie.image = item.movie.images.poster.thumb;
+
+                    itemList.push(movie);
+                }
+            }
+        }
+    })).then(function () {
+        return itemList;
+    });
 };
 
+Watchlist.prototype._load = function () {
+    var that = this;
+    delete localStorage.watchlist_fetched_time;
+    delete localStorage.watchlist_cached;
+    delete localStorage.watchlist_update_shows;
+    delete localStorage.watchlist_update_movies;
 
-module.exports = Vodo
+    var watchlist = [];
+
+    return that.trakt.ondeck.getAll().then(function (tv) {
+        console.log('shows fetched'); //debug
+        // store update data
+        localStorage.watchlist_update_shows = JSON.stringify(tv);
+
+        // add tv show to watchlist
+        watchlist = watchlist.concat(tv.shows);
+
+        return that.trakt.sync.watchlist.get({
+            extended: 'full,images',
+            type: 'movies'
+        });
+    }).then(function (movies) {
+        console.log('movies fetched'); //debug
+
+        // store update data
+        localStorage.watchlist_update_movies = JSON.stringify(movies);
+
+        // add movies to watchlist
+        watchlist = watchlist.concat(movies);
+
+        return format(watchlist);
+    }).then(rearrange).then(function (items) {
+        // store fetched timestamp
+        localStorage.watchlist_fetched_time = Date.now();
+
+        // cache watchlist
+        localStorage.watchlist_cached = JSON.stringify(items);
+
+        return {
+            results: items,
+            hasMore: false
+        };
+    });
+};
+
+Watchlist.prototype._update = function (id) {
+    var that = this;
+    var update_data = JSON.parse(localStorage.watchlist_update_shows);
+    delete localStorage.watchlist_fetched_time;
+    delete localStorage.watchlist_cached;
+    delete localStorage.watchlist_update_shows;
+
+    var watchlist = [];
+
+    return that.trakt.ondeck.updateOne(update_data, id).then(function (tv) {
+        console.log('shows updated'); //debug
+        // store update data
+        localStorage.watchlist_update_shows = JSON.stringify(tv);
+
+        // add tv show & movies to watchlist
+        watchlist = JSON.parse(localStorage.watchlist_update_movies).concat(tv.shows);
+
+        return format(watchlist);
+    }).then(rearrange).then(function (items) {
+        // store fetched timestamp
+        localStorage.watchlist_fetched_time = Date.now();
+
+        // cache watchlist
+        localStorage.watchlist_cached = JSON.stringify(items);
+
+        return {
+            results: items,
+            hasMore: false
+        };
+    });
+};
+
+Watchlist.prototype.extractIds = function (items) {
+    return _.pluck(items, 'imdb_id');
+};
+
+Watchlist.prototype.detail = function (torrent_id, old_data, callback) {
+    return {};
+};
+
+Watchlist.prototype.fetch = function (filters) {
+    var that = this;
+    return new Promise(function (resolve, reject) {
+        if (filters && typeof filters !== 'function' && (filters.force || filters.update)) {
+            if (filters.update && localStorage.watchlist_update_shows) {
+                console.error('Watchlist - update one item');
+                return that._update(filters.update).then(resolve).catch(reject);
+            } else {
+                if (filters.force) {
+                    console.error('Watchlist - force reload');
+                    return that._load().then(resolve).catch(reject);
+                } else {
+                    console.error('Watchlist - this should not be called', filters);
+                    reject('SHOULDNT BE CALLED');
+                }
+            }
+        } else {
+            // cache is 4 hours
+            if (!localStorage.watchlist_cached || localStorage.watchlist_fetched_time + that.cacheTime < Date.now()) {
+                console.error('Watchlist - no watchlist cached or cache expired');
+                if (that.trakt._authentication && that.trakt._authentication.access_token) {
+                    return that.fetch({force:true}).then(resolve).catch(reject);
+                } else {
+                    reject('Trakt not authenticated');
+                }
+            } else {
+                console.error('Watchlist - return cached');
+                resolve({
+                    results: JSON.parse(localStorage.watchlist_cached),
+                    hasMore: false
+                });
+            }
+        }  
+    });
+};
+
+module.exports = Watchlist;
